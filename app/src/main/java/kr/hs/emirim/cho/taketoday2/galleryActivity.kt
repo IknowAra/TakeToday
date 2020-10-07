15e287b754691903d14207ad7655611d4145ce47
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-10-08 03:27:56 +0900

    gallery ui update

--- app/src/main/java/kr/hs/emirim/cho/taketoday2/galleryActivity.kt
+++ app/src/main/java/kr/hs/emirim/cho/taketoday2/galleryActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_category.*
 
 class galleryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
