ae2f9e23f9e706acfb18c6065b108b14bf33093c
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-10-27 10:08:03 +0900

    hyeryung

--- app/src/main/java/kr/hs/emirim/cho/taketoday2/Popup_post.kt
+++ app/src/main/java/kr/hs/emirim/cho/taketoday2/Popup_post.kt
@@ -3,43 +3,38 @@ package kr.hs.emirim.cho.taketoday2
 import android.app.ActionBar
 import android.app.AlertDialog
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.core.content.ContextCompat.getSystemService
 
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.activity_popup_post.*
+import java.util.jar.Manifest
 
 class Popup_post : DialogFragment(){
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        var rootview: View=inflater.inflate(R.layout.activity_popup_post, container, false)
-//
-////        val params: ViewGroup.LayoutParams? = alertDialog?.window?.attributes
-//
-//        rootview.layoutParams=LinearLayout.LayoutParams(400,400)
-//        //rootview.layoutParams.width = 400
-//        //rootview.layoutParams.height = 400
-//        dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//        return rootview
-//
-//        val dialogView = layoutInflater.inflate(R.layout.activity_popup_post, null)
-//        val alertDialog = AlertDialog.Builder(Popup_post).create()
-//        alertDialog.setView(dialogView)
-//        alertDialog.show()
-//        alertDialog.window!!.setLayout(700, WindowManager.LayoutParams.WRAP_CONTENT)
-//    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.white_round);
+
+        set_image.setOnClickListener{
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                if(checkSelfPermission((activity as Popup_post).requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) !=PackageManager.PERMISSION_GRANTED){
+                    android.widget.Toast.makeText((activity as Popup_post).requireContext(), "Permission Denied", android.widget.Toast.LENGTH_SHORT).show();
+                    androidx.core.app.ActivityCompat.requestPermissions((activity as Popup_post).requireActivity(), new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                }
+            }
+        }
+
         return inflater.inflate(R.layout.activity_popup_post, container, false)
     }
 
