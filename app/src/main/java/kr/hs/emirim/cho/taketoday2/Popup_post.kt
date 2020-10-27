8b285bd7c17b3300397ef2ed823c237fcd450a33
kimara <s2019w06@e-mirim.hs.kr>
2020-10-27 10:06:38 +0900

    login 기능(auth)

--- app/src/main/java/kr/hs/emirim/cho/taketoday2/Popup_post.kt
+++ app/src/main/java/kr/hs/emirim/cho/taketoday2/Popup_post.kt
@@ -1,48 +1,73 @@
 package kr.hs.emirim.cho.taketoday2
 
-import android.app.ActionBar
-import android.app.AlertDialog
-import android.content.Context
-import android.graphics.Color
-import android.graphics.Point
-import android.graphics.drawable.ColorDrawable
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.LinearLayout
-import androidx.core.content.ContextCompat.getSystemService
-
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.activity_popup_post.*
 
 class Popup_post : DialogFragment(){
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        var rootview: View=inflater.inflate(R.layout.activity_popup_post, container, false)
-//
-////        val params: ViewGroup.LayoutParams? = alertDialog?.window?.attributes
-//
-//        rootview.layoutParams=LinearLayout.LayoutParams(400,400)
-//        //rootview.layoutParams.width = 400
-//        //rootview.layoutParams.height = 400
-//        dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//        return rootview
-//
-//        val dialogView = layoutInflater.inflate(R.layout.activity_popup_post, null)
-//        val alertDialog = AlertDialog.Builder(Popup_post).create()
-//        alertDialog.setView(dialogView)
-//        alertDialog.show()
-//        alertDialog.window!!.setLayout(700, WindowManager.LayoutParams.WRAP_CONTENT)
-//    }
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var currentPhotoPath: String
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.white_round);
+
+        image.setOnClickListener {
+            if(checkPersmission()){
+                dispatchTakePictureIntent()
+            }else{
+                requestPermission()
+            }
+        }
+
+
+
         return inflater.inflate(R.layout.activity_popup_post, container, false)
     }
 
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(PackageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+    //카메라 권한 요청
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE, CAMERA),
+                REQUEST_IMAGE_CAPTURE)
+    }
+    // 카메라 권한 체크
+    private fun checkPersmission(): Boolean {
+        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+    }
+    // 권한요청 결과
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d("TAG", "Permission: " + permissions[0] + "was " + grantResults[0] + "카메라 허가 받음 예이^^")
+        }else{
+            Log.d("TAG","카메라 허가 못받음 ㅠ 젠장!!")
+        }
+    }
     override fun onStart() {
         super.onStart()
         val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
