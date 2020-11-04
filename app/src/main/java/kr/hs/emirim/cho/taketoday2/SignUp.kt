e70b059b7aa2e12f88e8839ada2f1c40fe263cdc
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-05 02:50:20 +0900

    하는 중 썅2

--- app/src/main/java/kr/hs/emirim/cho/taketoday2/SignUp.kt
+++ app/src/main/java/kr/hs/emirim/cho/taketoday2/SignUp.kt
@@ -16,10 +16,10 @@ import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUp : AppCompatActivity() {
 
-    private lateinit var userEmail:String;
-    private lateinit var userPass:String;
+    private lateinit var userEmail: String;
+    private lateinit var userPass: String;
     private var current_user: FirebaseUser? = null
-    private lateinit var confirmPass:String;
+    private lateinit var confirmPass: String;
     private var user_name: String? = null
     private var user_tele: String? = null
     private var user_id: String? = null
@@ -33,34 +33,21 @@ class SignUp : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         mAuth = FirebaseAuth.getInstance()
-        current_user= mAuth.currentUser
+        current_user = mAuth.currentUser
 
-        btn_sign.setOnClickListener {
-
-            if(current_user!!.isEmailVerified){
-                Toast.makeText(
-                    this,
-                    "회원가입 완료 인증도",
-                    Toast.LENGTH_SHORT
-                ).show()
-                addUser()
-            }else{
-                Toast.makeText(
-                    this,
-                    "xx",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            userEmail=email.text.toString()
-            userPass=password.text.toString()
-            confirmPass=confirm_password.text.toString()
-            user_name=userName.text.toString()
-            user_tele=userTele.text.toString()
-            user_email = current_user?.email
-            if (!TextUtils.isEmpty(userEmail) && !TextUtils.isEmpty(userPass) && !TextUtils.isEmpty(confirmPass) && !TextUtils.isEmpty(user_name) && !TextUtils.isEmpty(user_tele)) {
+        btn_verify.setOnClickListener {
+            userEmail = email.text.toString()
+            userPass = password.text.toString()
+            confirmPass = confirm_password.text.toString()
+//            user_name=userName.text.toString()
+//            user_tele=userTele.text.toString()
+//            user_email = current_user?.email
+            if (!TextUtils.isEmpty(userEmail) && !TextUtils.isEmpty(userPass) && !TextUtils.isEmpty(
+                    confirmPass
+                ) && !TextUtils.isEmpty(user_name) && !TextUtils.isEmpty(user_tele)
+            ) {
                 if (userPass == confirmPass) {
                     reg_progress.visibility = View.VISIBLE
-                    user_id = current_user?.uid
                     mAuth.createUserWithEmailAndPassword(userEmail, userPass)
                         .addOnCompleteListener(OnCompleteListener<AuthResult?> { task ->
                             if (task.isSuccessful) {
@@ -71,15 +58,18 @@ class SignUp : AppCompatActivity() {
                                             "회원가입 완료 이메일 인증을 확인해주세요",
                                             Toast.LENGTH_SHORT
                                         ).show()
-                                        if(current_user!!.isEmailVerified){
-                                            Toast.makeText(
-                                                this,
-                                                "회원가입 완료 인증도",
-                                                Toast.LENGTH_SHORT
-                                            ).show()
-                                            addUser()
-                                        }
                                     }
+                                if (current_user!!.isEmailVerified) {
+                                    Toast.makeText(this, "인증됨", Toast.LENGTH_SHORT).show()
+                                    layout_setup.visibility = View.VISIBLE
+                                    addUser()
+                                } else {
+                                    Toast.makeText(
+                                        this,
+                                        "회원가입 완료 이메일 인증을 확인해주세요",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
                             } else {
                                 val errorMessage = task.exception!!.message
                                 Toast.makeText(
@@ -94,14 +84,17 @@ class SignUp : AppCompatActivity() {
                     Toast.makeText(this, "비밀번호를 다시 확인해주세요.", Toast.LENGTH_SHORT)
                         .show()
                 }
+
             }
+
+
         }
-
     }
-
     private fun addUser() {
         val db: FirebaseFirestore = FirebaseFirestore.getInstance()
         val user = User(user_name, user_tele)
+        current_user = mAuth.currentUser
+        user_id = current_user?.uid
 
         val userMap = hashMapOf(
             "email" to user_email.toString(),
@@ -111,7 +104,7 @@ class SignUp : AppCompatActivity() {
             "finish" to listOf<String>()
         )
 
-        Toast.makeText(this, "지금 이메일 : "+current_user?.email, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "지금 이메일 : " + current_user!!.email, Toast.LENGTH_SHORT).show()
         db.collection("Users").document(user_id.toString()).set(userMap)
             .addOnCompleteListener(OnCompleteListener<Void?> { task ->
                 if (task.isSuccessful) {
@@ -119,7 +112,7 @@ class SignUp : AppCompatActivity() {
                         .show()
                     startActivity(Intent(this, Login::class.java))
                     finish()
-                }else {
+                } else {
                     val error = task.exception!!.message
                     Toast.makeText(
                         this,
@@ -129,5 +122,6 @@ class SignUp : AppCompatActivity() {
                     Log.d("에러 : ", error!!)
                 }
             })
+
     }
 }
