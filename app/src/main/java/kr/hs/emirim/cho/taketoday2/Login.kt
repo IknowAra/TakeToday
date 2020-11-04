7607222d4c4f6cdab802cf1283945513921df4b7
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-04 23:19:42 +0900

    login 초기 실행 오류 해결 구현

--- app/src/main/java/kr/hs/emirim/cho/taketoday2/Login.kt
+++ app/src/main/java/kr/hs/emirim/cho/taketoday2/Login.kt
@@ -16,7 +16,7 @@ class Login : AppCompatActivity() {
 
     var db: FirebaseFirestore? = null
     private var mAuth: FirebaseAuth? = null
-    private var currentUser: FirebaseUser? = null
+    private var current_User: FirebaseUser? = null
     private var user_email: String?=null
     private var user_id: String? = null
 
@@ -26,13 +26,11 @@ class Login : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
-        currentUser= mAuth!!.currentUser
-        val user = mAuth!!.currentUser
-
-        user_email = user?.email
-
 
         login_btn.setOnClickListener(View.OnClickListener {
+            current_User= mAuth!!.currentUser
+            user_email=current_User?.email
+            Log.d("#)(*(#$)*#(*$)(============>>>>>>", user_email.toString())
             val loginEmail: String = login_email.getText().toString()
             val loginPass: String = login_password.getText().toString()
             if (!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPass)) {
@@ -73,11 +71,11 @@ class Login : AppCompatActivity() {
         super.onStart()
         Toast.makeText(this, "hihihihi", Toast.LENGTH_SHORT).show()
 
-        if (currentUser != null) {
-            user_id = currentUser!!.uid
+        if (current_User != null) {
+            user_id = current_User!!.uid
             sendToMain()
         } else {
-            Toast.makeText(this, "LoginActivity = > "+currentUser, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "LoginActivity = > "+current_User, Toast.LENGTH_SHORT).show()
         }
     }
 
