c80bc7af3b5ea3df80060791a4adcdd16b591822
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-12 03:09:01 +0900

    bmm

--- app/src/main/java/kr/hs/emirim/cho/taketoday2/galleryActivity.kt
+++ app/src/main/java/kr/hs/emirim/cho/taketoday2/galleryActivity.kt
@@ -55,37 +55,13 @@ class galleryActivity : AppCompatActivity() {
         remain = listOf()
         val db:FirebaseFirestore= FirebaseFirestore.getInstance()
 
-//        db.collection("Todays").whereEqualTo("cate",code).whereEqualTo("user",user_id.toString()).get().addOnSuccessListener { documents->
-//            for(d in documents){
-//                var now = d.data?.get(key = "now").toString().toInt()
-//                var a:List<Int> = d.data?.get(key = "remain") as List<Int>
-//            }
-//        }
-//        if(now in remain){
-//
-//        }else{
-//            Log.d("now=====>",now.toString())
-//            Log.d("remain=====>",remain.toString())
-//            end=System.currentTimeMillis()
-//
-//            db.collection("Todays").whereEqualTo("cate", code).whereEqualTo("user", user_id.toString()).get().addOnSuccessListener { documents->
-//                for(docu in documents){
-//                    start=docu.data.get(key="time").toString().toLong()
-//                }
-//            }
-//            getTime= (end-start)/1000
-//            Log.d("getTime====>",getTime.toString())
-//            if(getTime>=86400){
-//                makeRandom()
-//                Toast.makeText(this, "하루가 지나 주제가 변경되었습니다", Toast.LENGTH_SHORT).show()
-//                updateStart()
-//                val intent=Intent(this, galleryActivity::class.java)
-//                startActivity(intent)
-//                finish()
-//            }
-//        }
-
-
+        db.collection("Todays").whereEqualTo("cate",code).whereEqualTo("user",user_id.toString()).get().addOnSuccessListener { documents->
+            for(d in documents){
+                var noww = d.data?.get(key = "now").toString().toInt()
+                var a:List<Int> = d.data?.get(key = "remain") as List<Int>
+                HoursCate(noww, a)
+            }
+        }
 
         btn_reset.setOnClickListener {
             end=System.currentTimeMillis()
@@ -212,6 +188,33 @@ class galleryActivity : AppCompatActivity() {
 
      }
 
+    private fun HoursCate(noww: Int, a: List<Int>) {
+        val db:FirebaseFirestore= FirebaseFirestore.getInstance()
+        if(noww in a){
+
+        }else{
+            Log.d("now=====>",noww.toString())
+            Log.d("remain=====>",a.toString())
+            end=System.currentTimeMillis()
+
+            db.collection("Todays").whereEqualTo("cate", code).whereEqualTo("user", user_id.toString()).get().addOnSuccessListener { documents->
+                for(docu in documents){
+                    start=docu.data.get(key="time").toString().toLong()
+                }
+            }
+            getTime= (end-start)/1000
+            Log.d("getTime====>",getTime.toString())
+            if(getTime>=86400){
+                makeRandom()
+                Toast.makeText(this, "하루가 지나 주제가 변경되었습니다", Toast.LENGTH_SHORT).show()
+                updateStart()
+                val intent=Intent(this, galleryActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
     public fun makeRandom(){
         val db: FirebaseFirestore = FirebaseFirestore.getInstance()
         db.collection("Todays").whereEqualTo("cate",code).whereEqualTo("user",user_id).get().addOnSuccessListener { documents ->
@@ -243,14 +246,27 @@ class galleryActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        buttons = arrayListOf<Button>(findViewById(R.id.btn_1),findViewById(R.id.btn_2),findViewById(R.id.btn_3),findViewById(R.id.btn_4),findViewById(R.id.btn_5),findViewById(R.id.btn_6),findViewById(R.id.btn_7),findViewById(R.id.btn_8),findViewById(R.id.btn_9),findViewById(R.id.btn_10),findViewById(R.id.btn_11),findViewById(R.id.btn_12),findViewById(R.id.btn_13),findViewById(R.id.btn_14),findViewById(R.id.btn_15),findViewById(R.id.btn_16),findViewById(R.id.btn_17),findViewById(R.id.btn_18),findViewById(R.id.btn_19),findViewById<Button>(R.id.btn_20))
+
         mAuth = FirebaseAuth.getInstance()
         storageReference = FirebaseStorage.getInstance().reference
         val user= mAuth.currentUser
+        remain = listOf()
         user?.let{
             user_id = user.uid
         }
         remain = listOf()
+        val db:FirebaseFirestore= FirebaseFirestore.getInstance()
 
+        db.collection("Todays").whereEqualTo("cate",code).whereEqualTo("user",user_id.toString()).get().addOnSuccessListener { documents->
+            for(d in documents){
+                var noww = d.data?.get(key = "now").toString().toInt()
+                var a:List<Int> = d.data?.get(key = "remain") as List<Int>
+                Log.d("now=======>",noww.toString())
+                Log.d("aremain=======>",a.toString())
+                HoursCate(noww, a)
+            }
+        }
 
         btn_reset.setOnClickListener {
             end=System.currentTimeMillis()
@@ -258,18 +274,13 @@ class galleryActivity : AppCompatActivity() {
             db.collection("Todays").whereEqualTo("cate", code).whereEqualTo("user", user_id.toString()).get().addOnSuccessListener { documents->
                 for(docu in documents){
                     start=docu.data.get(key="time").toString().toLong()
-                    Log.d("start ======>", start.toString())
-                    Log.d("end ======>", end.toString())
-                    getTime= (end-start)/1000
+                    getTime= ((end-start)/1000)
                     getReset(getTime)
+
                 }
             }
 
         }
-
-
-
-        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
         db.collection("Users").document(user_id.toString()).get().addOnSuccessListener { document ->
             code = document.data?.get(key = "inCate").toString()
